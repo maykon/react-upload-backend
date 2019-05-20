545246f24f18e4b78dc3c901b5905d206c0eb961
Maykon Capellari <maykonluiscapellari@gmail.com>
2019-05-20 12:52:29 -0300

    Add helmet and cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
 const app = express();
 
+app.use(helmet());
+app.disable("x-powered-by");
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
