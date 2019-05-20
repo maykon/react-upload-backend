7ca259e7d9890df80aa4f2775ab129854f7a9725
Maykon Capellari <maykonluiscapellari@gmail.com>
2019-05-20 17:50:45 -0300

    Finish app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const compression = require("compression");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true
+});
+
+app.use(compression());
 app.use(helmet());
 app.disable("x-powered-by");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 app.use(require("./routes"));
 
 app.listen(3000);
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,32 @@
 const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
-routes.get("/", (req, res) => {
-  return res.json({ hello: "World" });
+const Post = require("./models/Post");
+
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  return res.json(posts);
+});
+
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, key, location: url = "" } = req.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+
+  return res.json(post);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  if (!post) return res.status(404).json({ error: "Post not found" });
+  await post.remove();
+  return res.send();
 });
 
 module.exports = routes;
